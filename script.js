e1b16d5de3a63efd7b639997b01d9cc3ab91f70e
PSaveika <101928958+PSaveika@users.noreply.github.com>
2022-07-26 01:23:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict"
 
-//praktiskai, klase yra svaresnis ir aiskesnis objekto aprasymas?
-//kaip suprantu, galima būtų daryt ir su object, bet reiktu apsirašyt kiekvieną mygtuką
-//kas būtųlaiko švaistymas ir neefektyvus kodo rašymas, todėl ir naujama klasė, kad nereiktų
-//kartoti kodo?
 //sukurian nauja klasės atvejį (naują klasę), tai sukuriam naują objektą ir kai jį sukuriam, per klasę perduodam parametrus, per konstruktorių
 
 class Calculator {
@@ -13,8 +9,38 @@ class Calculator {
         this.clearInput();
     }
 
+    getNumber(number) {
+        //additional function for number separation
+        const stringNumber = number.toString(); //convert to string for number splitting on .
+        const integerNumbers = parseFloat(stringNumber.split('.')[0]); //[0] - first portion of array
+        const decimalNumbers = stringNumber.split('.')[1]; //[1] - second portion of array
+        
+        var integerDisplay;
+
+        //if not a number (if . or nothing )
+        if(isNaN(integerNumbers)) {
+            integerDisplay = '';
+        } else {
+            //maximumFractionDigits: 0 - to be sure that there isn't any decimal places
+            integerDisplay = integerNumbers.toLocaleString('en', {maximumFractionDigits: 0});
+        }
+        //if there is any decimal . 
+        if(decimalNumbers != null) {
+            return `${integerDisplay}.${decimalNumbers}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
     updateDisplay() {
-        this.currentInput.innerText = this.currentDisplayedInput;
+        this.currentInput.innerText = this.getNumber(this.currentDisplayedInput);
+        //if there is an operation, show previous input number and the operand symbol
+        if(this.operand != null) {
+            this.previousInput.innerText = `${this.getNumber(this.previousDisplayedInput)} ${this.operand}`;
+        } else {
+            this.previousInput.innerText = ''
+        }
+        
     }
 
     clearInput() {
@@ -31,16 +57,57 @@ class Calculator {
         this.currentDisplayedInput = this.currentDisplayedInput.toString() + number.toString();
     }
 
-    getOperandOnClick(operation){
-
+    getOperandOnClick(operand){
+        //if current input empty - when don't let execute any further code
+        if(this.currentDisplayedInput === ''){
+            return;
+        }
+        //if previous operand not empty - when do the calculation
+        if(this.previousDisplayedInput !== '') {
+            this.calculate();
+        }
+        this.operand = operand;
+        //while done typing current numbers, recycle, clear the current input, and move it to the upper part of display
+        this.previousDisplayedInput = this.currentDisplayedInput;
+        this.currentDisplayedInput = '';
     }
 
     deleteOneNumberAtTime(){
+        //slice last value from string and delete - from start of the string (0) all the way to the second last number (-1)
+        this.currentDisplayedInput = this.currentDisplayedInput.toString().slice(0, -1);
 
     }
 
     calculate() {
-
+        var calculation; //result
+        var previousInputValue = parseFloat(this.previousDisplayedInput);
+        var currentInputValue = parseFloat(this.currentDisplayedInput);
+        //if one of the inputs empty - cancel function
+        if(isNaN(previousInputValue) || isNaN(currentInputValue)) {
+            return;
+        }
+        //switch - do many statements on single object - similar to if
+        switch (this.operand) {
+            //if operand '+' - break to not follow ant other case statements
+            case '+':
+                calculation = previousInputValue + currentInputValue;
+                break;
+            case '-':
+                calculation = previousInputValue - currentInputValue;
+                break;
+            case '*':
+                calculation = previousInputValue * currentInputValue;
+                break;
+            case '/':
+                calculation = previousInputValue / currentInputValue;
+                break;
+            //in none of the symbols match the operation - then just don't do any calculation
+            default:
+                return;
+        }
+        this.currentDisplayedInput = calculation;
+        this.operand = undefined;
+        this.previousDisplayedInput = '';
     }
 }
 
@@ -61,8 +128,8 @@ numberButtons.forEach(button => {
         //add the number, whatever is inside that button
         calculator.getNumberOnClick(button.innerText);
         calculator.updateDisplay();
-    })
-})
+    });
+});
 
 operandButtons.forEach(button => {
     //whenever I click on the button, I want do something
@@ -70,8 +137,25 @@ operandButtons.forEach(button => {
         //add the number, whatever is inside that button
         calculator.getOperandOnClick(button.innerText);
         calculator.updateDisplay();
-    })
-})
+    });
+});
+
+equalButton.addEventListener("click", button => {
+    calculator.calculate();
+    calculator.updateDisplay();
+});
+
+clearButton.addEventListener("click", button => {
+    calculator.clearInput();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", button => {
+    calculator.deleteOneNumberAtTime();
+    calculator.updateDisplay();
+});
+
+
 
 // the 'new' keyword
 // - creates a new empty object {}
@@ -80,4 +164,4 @@ operandButtons.forEach(button => {
 
 //constructor or constructor function - responsible for creating new object. It's just for object properties
 
-//method - aprašomi po konstruktorium
+//method - after constructor
